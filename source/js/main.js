882b36a4ac86d8505c84134fc98672e493158945
Polimezar <hero.twilight@mail.ru>
2020-10-22 17:15:01 +0300

    Делает стилизацию блоков тренеры +блок преимущества,акции и отзывы

--- source/js/main.js
+++ source/js/main.js
@@ -26,3 +26,31 @@ if (subscriptionLinks.length) {
     });
   });
 }
+
+var trainerSlider = new Swiper('.trainers__inner', {
+  breakpoints: {
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+  },
+  navigation: {
+    nextEl: '.trainers__button-next',
+    prevEl: '.trainers__button-previous',
+  },
+});
+
+var reviewsSliders = new Swiper('.reviews__block', {
+  navigation: {
+    nextEl: ".reviews__button-next",
+    prevEl: ".reviews__button-previous",
+  },
+})
